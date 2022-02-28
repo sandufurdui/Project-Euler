0b59f9f139b26a37a628741498acf0865ce72978
Alex Furdui <furdui.alexandru@isa.utm.md>
2022-02-28 20:09:11 +0200

    added even fibonacci numbers

---
+++ Even Fibonacci numbers/main.py
@@ -0,0 +1,15 @@
+
+
+
+
+if __name__ == '__main__':
+    x1 = 1
+    x2 = 1
+    s = 0
+    while x2 < 4000000:
+        if x2 % 2 == 0: s += x2
+        x3 = x1 + x2
+        # print(x3)
+        x1 = x2
+        x2 = x3
+    print(s)
