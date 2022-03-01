efa810ed984f14ccd5aa77884f7d79ec86abab86
Alex Furdui <furdui.alexandru@isa.utm.md>
2022-03-01 02:09:38 +0200

    added summation of primes

--- Summation of primes/main.py
+++ Summation of primes/main.py
@@ -1,5 +1,5 @@
-from custom_lib import generate_primes_under_value
 from custom_lib import sum_list
+from custom_lib import generate_primes_under_value
 
 if __name__ == '__main__':
     k = 2000000
